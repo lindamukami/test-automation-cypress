d260fec6e5d1b14dafc026f13c9ba05f52331024
Linda Mukami <lindamukami32@gmail.com>
2022-10-15 11:46:11 +0300

    added  selectors for the selenium test page

---
+++ cypress/selectors/index.js
@@ -0,0 +1,24 @@
+export const SELENIUM_TEST_PAGES = {
+    ATTRIBUTES_TEST_PAGE: {
+        PARAGRAPH_WITH_DYNAMIC_ATTRIBUTES: '#jsattributes',
+        BUTTON: 'button.styled-click-button'
+    },
+    TABLE_TEST_PAGE: {
+        TABLE_SECTION: '#mytable',
+        TABLE_ROW: 'tr',
+    },
+    BASIC_JAVASCRIPT_VALIDATION_TEST_PAGE: {
+        INPUT_FIELD: '#lteq30',
+        INPUT_SUBMIT: 'input.styled-click-button',
+
+    },
+    BASIC_TEST_PAGE: {
+        PAGE_BODY: '.page-body',
+        NAVIGATION_INDEX_LINK: '.page-navigation a',
+        H1_HEADING: 'h1',
+        PARAGRAPH: 'p',
+        EXPLANATION_SECTION: '.explanation',
+        FOOTER: '.page-footer',
+
+    },
+};
