14cc57909d577a518888950a3ece87b097c6679b
Linda Mukami <lindamukami32@gmail.com>
2022-10-15 11:52:03 +0300

    added pathnames for the selenium test pages

---
+++ cypress/constants/index.js
@@ -0,0 +1,14 @@
+export const HTML = 'HTML';
+export const PHP = 'PHP';
+
+export const SELENIUM_TEST_PAGES = {
+    BASE_URL: 'https://testpages.herokuapp.com/styled',
+    PATHNAMES: {
+        INDEX_PAGE: '/index',
+        BASIC_WEB_PAGE: '/basic-web-page-test',
+        ATTRIBUTES_PAGE: '/attributes-test',
+        TABLE_PAGE: '/tag/table',
+        BASIC_JAVA_SCRIPT_VALIDATION_PAGE: '/basic-javascript-validation-test',
+
+    },
+};
